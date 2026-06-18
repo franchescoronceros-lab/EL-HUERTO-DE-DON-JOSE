67d27b498cece667a4c065c8b6fe74cdfd546757
franchescoronceros-lab <franchescoronceros@gmail.com>
2026-06-18 17:47:04 -0500

    Actualizacion de rutas y controladores

--- SISTEMA/farmacia_SJB/app/Http/Controllers/dashboardController.php
+++ SISTEMA/farmacia_SJB/app/Http/Controllers/dashboardController.php
@@ -6,8 +6,5 @@ use Illuminate\Http\Request;
 
 class dashboardController extends Controller
 { 
-    public fuction index()
-    {
-        return view('TEMPLATES.administrador');
-    }
+    
 }
--- SISTEMA/farmacia_SJB/routes/web.php
+++ SISTEMA/farmacia_SJB/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\dashboardController;
+use App\Http\Controllers\configuracionesController;
 
+use App\Http\Controllers\categoriesController;
+use App\Http\Controllers\medicinesController;
+use App\Http\Controllers\customerController;
+use App\Http\Controllers\salesController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,4 +23,12 @@ use Illuminate\Support\Facades\Route;
 //    return view('welcome');
 //});
 
+Route::get('/',[dashboardController::class, 'index'])->name('dashboard.index');
+Route::get('configuraciones', [configuracionesController::class, 'index'])->name('configuaciones.index');
+
 Route::get('/', [dashboardController::class, 'index'])->name('dashboard.index');
+
+Route::resource('categories', categoriesController::class);
+Route::resource('medicines', medicinesController::class);
+Route::resource('customer', customersController::class);
+Route::resource('sales', salesController::class);
